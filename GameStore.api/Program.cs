14ac2be42932ac87cd6aa50ced6fd905c3466353
qasim <qassim9590@hotmail.com>
2024-05-01 10:59:29 +0300

    better CRUD

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -19,7 +19,15 @@ app.MapGet("games", () => games);
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id)).WithName(GetGameEndpointName);
+app.MapGet("games/{id}", (int id) =>
+{
+    GameDto? game = games.Find(game => game.Id == id);
+
+    return game is null ? Results.NotFound() : Results.Ok(game);
+
+}
+
+).WithName(GetGameEndpointName);
 
 // POST
 app.MapPost("games", (CreateGameDto newGame) =>
@@ -39,6 +47,11 @@ app.MapPost("games", (CreateGameDto newGame) =>
 app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
 {
     var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound();
+    }
 
     games[index] = new GameDto(id, updatedGame.Name, updatedGame.Genre, updatedGame.Price, updatedGame.ReleaseDate);
 
